69c08706974f4b74761472b89a9b2f72badf8e07
Islam Saeed <islamsaeed111998@gmail.com>
2023-10-13 13:23:09 -0700

    category menu

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -1,12 +1,32 @@
+'use client'
 import React from 'react'
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
+import { Button } from '@mui/material';
+import CategoryMenu from './CategoryMenu';
 
 const Categories = () => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };  
   return (
     <Grid container spacing={2} justifyContent='space-around' sx={{pl:2, pr:2, pt:4, pb:2}}>
       <Grid item>
-        <Typography variant="subtitle1">Woman</Typography>
+        <Button
+          id="basic-button"
+          aria-controls={open ? 'basic-menu' : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? 'true' : undefined}
+          onClick={handleClick}
+        >
+          Woman
+        </Button>
+        <CategoryMenu anchorEl={anchorEl} open={open} handleClick={handleClick} handleClose={handleClose} />
       </Grid>
       <Grid item>
         <Typography variant="subtitle1">Man</Typography>
---
+++ components/CategoryMenu.tsx
@@ -0,0 +1,108 @@
+import React from 'react'
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Grid from '@mui/material/Grid'
+import Image from 'next/image';
+import { Typography } from '@mui/material';
+
+type MenuProps = {
+  anchorEl: null | HTMLElement,
+  open: boolean,
+  handleClick: (event: React.MouseEvent<HTMLButtonElement>) => void,
+  handleClose: () => void
+}
+
+
+const CategoryMenu = ({anchorEl, open, handleClick, handleClose}:MenuProps) => {
+  return (
+    <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <Grid container spacing={5} sx={{p:5}}>
+          <Grid item>
+            <Grid container spacing={3}>
+              <Grid item>
+                <Grid container spacing={6} direction='column'>
+                  <Grid item>
+                    <Typography variant="h6" sx={{mb:'10px', pl:2}}>Shoe & Bag</Typography>
+                    <MenuItem onClick={handleClose}>Casual Shoes</MenuItem>
+                    <MenuItem onClick={handleClose}>Boots</MenuItem>
+                    <MenuItem onClick={handleClose}>Sandals</MenuItem>
+                    <MenuItem onClick={handleClose}>Slippers</MenuItem>
+                  </Grid>
+                  <Grid item>
+                    <Typography variant="h6" sx={{mb:'10px', pl:2}}>Luxury & designer</Typography>
+                    <MenuItem onClick={handleClose}>Towels</MenuItem>
+                    <MenuItem onClick={handleClose}>Bathroom Scales</MenuItem>
+                    <MenuItem onClick={handleClose}>Bath Mats</MenuItem>
+                    <MenuItem onClick={handleClose}>Shower Caps</MenuItem>
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item>
+                <Grid container spacing={6} direction='column'>
+                  <Grid item>
+                    <Typography variant="h6" sx={{mb:'10px', pl:2}}>Home Textile</Typography>
+                    <MenuItem onClick={handleClose}>Bedding</MenuItem>
+                    <MenuItem onClick={handleClose}>Pillows</MenuItem>
+                    <MenuItem onClick={handleClose}>Handkerchief Towels</MenuItem>
+                    <MenuItem onClick={handleClose}>Curtain</MenuItem>
+                  </Grid>
+                  <Grid item>
+                    <Typography variant="h6" sx={{mb:'10px', pl:2}}>Cosmetics</Typography>
+                    <MenuItem onClick={handleClose}>Shampoo and Conditioner</MenuItem>
+                    <MenuItem onClick={handleClose}>Styling Products</MenuItem>
+                    <MenuItem onClick={handleClose}>Hair Accessories</MenuItem>
+                    <MenuItem onClick={handleClose}>Men&apos;s Grooming</MenuItem>
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item>
+                <Grid container spacing={6} direction='column'>
+                  <Grid item>
+                    <Typography variant="h6" sx={{mb:'10px', pl:2}}>Party Supplies</Typography>
+                    <MenuItem onClick={handleClose}>Event &amp; Party</MenuItem>
+                    <MenuItem onClick={handleClose}>Christmas</MenuItem>
+                    <MenuItem onClick={handleClose}>Artificial Decorations</MenuItem>
+                    <MenuItem onClick={handleClose}>Wedding</MenuItem>
+                  </Grid>
+                  <Grid item>
+                    <Typography variant="h6" sx={{mb:'10px', pl:2}}>Sport &amp; Outdoors</Typography>
+                    <MenuItem onClick={handleClose}>Team Sports</MenuItem>
+                    <MenuItem onClick={handleClose}>Water Sports</MenuItem>
+                    <MenuItem onClick={handleClose}>Outdoor Recreation</MenuItem>
+                    <MenuItem onClick={handleClose}>Fitness Equipment</MenuItem>
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item>
+                <Typography variant="h6" sx={{mb:'10px', pl:2}}>Clothes</Typography>
+                <MenuItem onClick={handleClose}>Bottoms</MenuItem>
+                <MenuItem onClick={handleClose}>Women&apos;s Clothing</MenuItem>
+                <MenuItem onClick={handleClose}>T-Shirts and Tops</MenuItem>
+                <MenuItem onClick={handleClose}>Dresses</MenuItem>
+                <MenuItem onClick={handleClose}>Outerwear</MenuItem>
+                <MenuItem onClick={handleClose}>Formal Wear</MenuItem>
+                <MenuItem onClick={handleClose}>Casual Wear</MenuItem>
+                <MenuItem onClick={handleClose}>Seasonal Collections</MenuItem>
+                <MenuItem onClick={handleClose}>Sports Bras</MenuItem>
+                <MenuItem onClick={handleClose}>Workout Tops</MenuItem>
+                <MenuItem onClick={handleClose}>Fall Wardrobe</MenuItem>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Image src='https://source.unsplash.com/278x340' width={278} height={340} alt='offer' />
+          </Grid>
+        </Grid>
+      </Menu>
+  )
+}
+
+export default CategoryMenu
