fc51df8730a4420878a7ebf4179cdb99468e60d4
Islam Saeed <islamsaeed111998@gmail.com>
2023-10-30 20:44:39 -0700

    category header

--- app/[category]/[subcategory]/page.tsx
+++ app/[category]/[subcategory]/page.tsx
@@ -1,12 +1,14 @@
 import CardGrid from '@/components/CardGrid'
 import TopCard from '@/components/TopCard'
 import BreadCrumb from '@/components/BreadCrumb'
+import CategoryHeader from '@/components/CategoryHeader'
 import React from 'react'
 
 const page = () => {
   return (
     <>
       <BreadCrumb />
+      <CategoryHeader />
       <CardGrid>
           <TopCard />
           <TopCard />
---
+++ components/CategoryHeader.tsx
@@ -0,0 +1,63 @@
+'use client'
+import React from 'react'
+import Grid from '@mui/material/Grid'
+import { usePathname } from 'next/navigation'
+import { TextField, Typography, MenuItem } from '@mui/material'
+
+
+type keyValuePair = {
+    value: string,
+    label: string
+}
+
+const filter:Array<keyValuePair> = [
+    {
+      value: "Sort by order",
+      label: "Sort by order"
+    },
+    {
+      value: 'All Categories',
+      label: 'All Categories'
+    }
+  ];
+
+const CategoryHeader = () => {
+    const pathname = usePathname()
+    let category = [...pathname.slice(1).split('/')][0]
+  return (
+    <Grid container justifyContent='space-between' alignItems='center' sx={{height:'80px', backgroundColor:'#E9E9E9', px:8, mb:5}}>
+        <Grid item>
+            <Grid container justifyContent='space-between' alignItems='center' sx={{width: '180px'}}>
+              <Grid item>
+                <Typography variant="h5" sx={{height:'50px', textAlign:'center', display:'table-cell', verticalAlign:'middle'}}>{category}</Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="subtitle1" sx={{height:'50px', textAlign:'center', display:'table-cell', verticalAlign:'middle'}}>110 items</Typography>
+              </Grid>
+            </Grid>
+        </Grid>
+        <Grid item>
+            <TextField
+                id="fliter-selection"
+                select
+                defaultValue="Sort by order"
+                variant="standard"
+                sx={{
+                border: 'none'
+                }}
+                InputProps={{
+                    disableUnderline: true,
+                }}
+            >
+                {filter.map((option:keyValuePair) => (
+                    <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                    </MenuItem>
+                ))}
+            </TextField>
+        </Grid>
+    </Grid>
+  )
+}
+
+export default CategoryHeader
