304353c4626d6b6d032bffa34cdbb8a76ba40472
Islam Saeed <islamsaeed111998@gmail.com>
2023-11-13 20:38:06 -0800

    product page and image gallery

---
+++ app/[category]/[subcategory]/[product]/page.tsx
@@ -0,0 +1,14 @@
+import BreadCrumb from '@/components/BreadCrumb'
+import ImageGallery from '@/components/ImageGallery'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+        <BreadCrumb />
+        <ImageGallery />
+    </>
+  )
+}
+
+export default page
---
+++ components/ImageGallery.tsx
@@ -0,0 +1,64 @@
+'use client'
+import * as React from 'react';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+
+function srcset(image: string, size: number, rows = 1, cols = 1) {
+  return {
+    src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
+    srcSet: `${image}?w=${size * cols}&h=${
+      size * rows
+    }&fit=crop&auto=format&dpr=2 2x`,
+  };
+}
+
+export default function ImageGallery() {
+  return (
+    <ImageList
+      sx={{ width: 620, height: 620 }}
+      variant="quilted"
+      cols={2}
+      rowHeight={120}
+    >
+      {itemData.map((item) => (
+        <ImageListItem key={item.img} cols={item.cols || 1} rows={item.rows || 1}>
+          <img
+            {...srcset(item.img, 120, item.rows, item.cols)}
+            alt={item.title}
+            loading="lazy"
+          />
+        </ImageListItem>
+      ))}
+    </ImageList>
+  );
+}
+
+const itemData = [
+  {
+    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+    title: 'Breakfast',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+    title: 'Burger',
+    rows: 5,
+    cols: 1,
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+    title: 'Camera',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+    title: 'Coffee',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
+    title: 'Hats',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
+    title: 'Honey',
+    author: '@arwinneil',
+  }
+];
